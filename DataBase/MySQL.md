9a79d739b3606173c31cf88e83dca26c1e40786c
yxd <2457800495@qq.com>
2020-04-22 17:23:05 +0800

    优化blog类目2

---
+++ DataBase/MySQL.md
@@ -0,0 +1,8 @@
+# 系统集成项目管理工程师
+
+## 第一章
+这是第一章的内容
+
+##第二章
+
+![图片](https://hi-yee.github.io/coconut-blog/images/imagesProjectManagement/shiwu_1.png)
---
+++ OperatingSystem/Linuxpush.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+message=$1
+
+if [[ $message = "" ]];
+then
+echo "请输入commit 参数"
+exit
+fi
+
+#当前md名称
+now_book="ProgrammingLanguage.md"
+#cp的文件名称
+book="2020-04-22-my-$now_book"
+#中间bak文件名
+book_bak="2020-04-22-my-bak-$now_book"
+
+coconut_path="../../hi-yee.github.io/_posts"
+
+#更新自己的分支
+echo "更新开始！"
+cp $now_book $coconut_path/$book
+git add ./
+git commit -m "$message"
+git push 
+
+echo "更新完成！"
+echo "更新博客开始！"
+
+cd $coconut_path
+cp $book $book_bak
+
+#添加固定的头
+echo "---
+layout:     post                    # 使用的布局（不需要改）
+title:      Python              # 标题
+subtitle:   Python 零基础入门学习Python     #副标题
+date:       2020-04-22              # 时间
+author:     coconut                      # 作者
+header-img:    #这篇文章标题背景图片
+catalog: true                       # 是否归档
+categories: 编程语言
+tags:                               #标签
+    - 学习
+---" > $book 
+#写入内容
+cat $book_bak >> $book
+#干掉备份文件
+rm -f $book_bak
+git add ./
+git commit -m "$message"
+git push origin master
+echo "更新博客结束！"
